ddb1aebbae65c4c26bd35f93264449532f58356f
Benjamin Radovsky <radovskyb@gmail.com>
2016-10-02 16:21:28 +1100

    added TODO

--- watcher.go
+++ watcher.go
@@ -281,6 +281,13 @@ func hasOption(option Option, options []Option) bool {
 	return false
 }
 
+// TODO: Make sure directories and files that are multiple layers deep still have
+//		 a unique path structure in the fileList map if they start with the same
+//		 path names and folders.
+//
+//		 Simple fix, add full directory path structure string and append before
+//		 info.Name() instead of just currentDir.
+//
 // ListFiles returns a map of all os.FileInfo's recursively
 // contained in a directory. If name is a single file, it returns
 // an os.FileInfo map containing a single os.FileInfo.
