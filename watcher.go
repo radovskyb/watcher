19766d7426993f29e0b597fd320f0f94476448fc
Benjamin Radovsky <radovskyb@gmail.com>
2016-10-21 18:57:10 +1100

    fix spelling

--- watcher.go
+++ watcher.go
@@ -21,7 +21,7 @@ var (
 )
 
 // An EventType is a type that is used to describe what type
-// of event has occured during the watching process.
+// of event has occurred during the watching process.
 type EventType int
 
 // EventTypes
@@ -46,14 +46,14 @@ const (
 
 // An Event desribes an event that is received when files or directory
 // changes occur. It includes the os.FileInfo of the changed file or
-// directory and the type of event that's occured and the full path of the file.
+// directory and the type of event that's occurred and the full path of the file.
 type Event struct {
 	EventType
 	Path string
 	os.FileInfo
 }
 
-// String returns a string depending on what type of event occured and the
+// String returns a string depending on what type of event occurred and the
 // file name associated with the event.
 func (e Event) String() string {
 	pathType := "FILE"
