72f282c3bc5573bff9a0fd3c307466a7a827ec0b
Benjamin Radovsky <radovskyb@gmail.com>
2016-11-05 22:02:42 +1100

    ...

--- watcher.go
+++ watcher.go
@@ -227,7 +227,7 @@ func (w *Watcher) Add(name string) error {
 func (w *Watcher) Remove(name string) error {
 	if name == "." || name == ".." {
 		var err error
-		name, err = filepath.Abs(filepath.Clean(name))
+		name, err = filepath.Abs(name)
 		if err != nil {
 			return err
 		}
